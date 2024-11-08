3f60d5416e22e7bf579b16fd9745cbb96c6a3f3a
Juan Villa <jujotovil@gmail.com>
2024-11-08 14:58:31 -0500

    funcionalidad transaccion

--- src/app/indexDashboard.js
+++ src/app/indexDashboard.js
@@ -1,3 +1,6 @@
+import {usuarios} from '../data/usuarioData.js'
+
+
 const operationTransferir = document.getElementById('operation__transferir')
 const operationConsignar = document.getElementById('operation__consignar')
 const operationRetirar = document.getElementById('operation__retirar')
@@ -11,18 +14,34 @@ const movimientos = document.getElementById('movimientos')
 
 const operations = [transferir, consignar, retirar, movimientos];
 
+function salarioTransaccion(salarioUsuario){
+    usuarios.forEach(usuario => {
+        if(salarioUsuario > usuario.saldo){
+            
+        }
+    })
+}
+
 function toggleFunction(operation__item) {
     operations.forEach(op => {
         if (op !== operation__item) {
             op.classList.add('hidden');
         } 
         else {
-            op.classList.remove('hidden');
+            op.classList.toggle('hidden');
         }
     });
 }
 operationTransferir.addEventListener('click', () => {
     toggleFunction(transferir)
+    
+    let cuentaDestino = document.getElementById('cuentaDestino').value;
+
+    let salarioATransferir = document.getElementById('salarioTransferir').value;
+
+    salarioTransaccion(salarioATransferir);
+
+
 })
 
 operationConsignar.addEventListener('click', () => {
@@ -36,3 +55,7 @@ operationRetirar.addEventListener('click', () => {
 operationMovimientos.addEventListener('click', () => {
     toggleFunction(movimientos)
 })
+
+
+
+console.log(usuarios);
---
+++ src/data/usuarioData.js
@@ -0,0 +1,20 @@
+// Simulador de datos de usuario
+
+export let usuarios = [
+    
+    {
+        id: 1,
+        nombreCuenta: "cuenta123",
+        contraseña: "hola12",
+        saldo: 200000,
+    },
+        
+    {
+        id: 2,
+        nombreCuenta: "cuenta321",
+        contraseña: "hola32",
+        saldo: 200000,
+    },
+        
+
+]
